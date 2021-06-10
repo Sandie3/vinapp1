8c4f92af590f3c321f0e833d926b6031c81db1da
Sandie3 <sandermunkh@gmail.com>
2021-06-10 13:24:03 +0200

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,17 +1,9 @@
-<<<<<<< Updated upstream
 import { Switch, Route, Fragment } from 'react'
 import { BrowserRouter, Link } from 'react-router-dom'
 
 import { postBooking } from '../apicall/bookings'
 
 import ContactOption from '../components/OptionContact'
-=======
-import { Switch, Route } from 'react'
-import { BrowserRouter as Router, Switch, Route, Link, useParams, useRouteMatch } from "react-router-dom";
-
-import { postBooking } from '../apicall/bookings'
-
->>>>>>> Stashed changes
 
 const Contact = () => {
 
@@ -20,15 +12,9 @@ const Contact = () => {
     
     
     return (
-<<<<<<< Updated upstream
-        <Fragment>
+        <Router>
             <div className="Contact">
                 <h1>Contact Information</h1><br /><br />
-=======
-    <Router>
-        <div className="Contact">
-            <h1>Contact Information</h1><br /><br />
->>>>>>> Stashed changes
 
                 <p>Contact for booking, canceling, feedback and other. <br /><br />
 
@@ -36,45 +22,26 @@ const Contact = () => {
 
                 Christian Kirkegaard:<br />
                 ck@bookvino.com<br />
-<<<<<<< Updated upstream
-                Mobile: +45 29 87 57 22<br /><br />
+                Mobile: +45 29 87 57 22<br /><br /> 
                 </p>
                 <div className="dropDown">
                     <h3>Choose<span>*</span>:</h3>
                     <div className="searchBarContainer">
                         <select name="" id="searchBarSelect">
-                            <option value="0"><Link to="/">Contact</Link></option>
-                            <option value="0">Booking</option>
-                            <option value="0">Cancel</option>
+                            <option value="0"><Link to={`${url}/message`}>Contact...</Link></option>
+                            <option value="1"><Link to={`${url}/booking`}>Booking</Link></option>
+                            <option value="2"><Link to={`${url}/cancel`}>Cancel</Link>Cancel</option>
                         </select>
                     </div>
-=======
-                Mobile: +45 29 87 57 22<br /><br /> 
-            </p>
-            <div className="dropDown">
-                <h3>Choose<span>*</span>:</h3>
-                <div className="searchBarContainer">
-                    <select name="" id="searchBarSelect">
-                        <option value="0"><Link to={`${url}/message`}>Contact...</Link></option>
-                        <option value="1"><Link to={`${url}/booking`}>Booking</Link></option>
-                        <option value="2"><Link to={`${url}/cancel`}>Cancel</Link>Cancel</option>
-                    </select>
->>>>>>> Stashed changes
-                </div>
 
+                </div>
+                <Switch>
+                    <Route path={`${path}/:page`}>
+                        <stuff />
+                    </Route>
+                </Switch>
             </div>
-<<<<<<< Updated upstream
-        </Fragment>
-=======
-            <Switch>
-                <Route path={`${path}/:page`}>
-                    <stuff />
-                </Route>
-            </Switch>
-                
-        </div>
-    </Router>
->>>>>>> Stashed changes
+        </Router>
     )
 }
 
