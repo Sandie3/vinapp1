7a64954a3636dfc8755e02a9def83e6fa547f48c
Sandie3 <sandermunkh@gmail.com>
2021-06-10 10:32:31 +0200

    Api call

---
+++ src/apicall/bookings.js
@@ -0,0 +1,55 @@
+import axios from 'axios';
+
+const api = {
+    baseUrl: 'http://localhost:5001/booking'
+}
+
+
+
+export const getBooking = () => {
+    let response = axios.get(api.baseUrl)
+        .then(res => { return res.data })
+    return response;
+}
+
+
+
+export const getBookingFromId = (bookingId) => {
+    let response = axios.get(api.baseUrl + '/' + bookingId)
+        .then(res => { return res.data })
+    return response;
+}
+
+
+
+export const searchBooking = (bookingId) => {
+    let response = axios.get(api.baseUrl + '/search/' + bookingId)
+        .then(res => { return res.data })
+    return response;
+}
+
+
+
+export const postBooking = (bookingData) => {
+    let response = axios.post(api.baseUrl + '/admin', bookingData)
+        .then(res => { return res.data })
+    return response;
+
+}
+
+
+
+export const delBooking = (bookingId) => {
+    let response = axios.delete(api.baseUrl + '/admin/' + bookingId)
+        .then(res => { return res.data })
+    return response;
+
+}
+
+
+
+export const editMonark = (bookingId, bookingData) => {
+    let response = axios.put(api.baseUrl + '/admin/' + bookingId, bookingData)
+        .then(res => { return res.data })
+    return response;
+}
