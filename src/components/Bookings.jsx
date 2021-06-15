813be0cbfb887b7ea83980d896f0d58212db7c96
rune-skaarup <79456478+rune-skaarup@users.noreply.github.com>
2021-06-15 10:24:16 +0200

    Booking

--- src/components/Bookings.jsx
+++ src/components/Bookings.jsx
@@ -2,8 +2,28 @@ import React from 'react'
 
 const Bookings = () => {
     return (
-        <div>
-            
+        <div className="bookingOption">
+            <div className="bookingText">
+                <h3>Name<span>*</span>:</h3> <br />
+                <input type="text" placeholder="Write your name..." /><br /><br />
+                <h3>Email<span>*</span>:</h3><br />
+                <input type="text" placeholder="Write your Email..." /><br /><br />
+                <h3>Phone<span>*</span>:</h3><br />
+                <input type="text" placeholder="Write your phone number..." />
+            </div>
+            <div className="Winery">
+                <h3>Choose winery<span>*</span>:</h3><br />
+                <select name="Booking" id="selectBooking">
+                    <option value="0">Winery1</option>
+                    <option value="1">Winery2</option>
+                    <option value="2">Winery3</option>
+                </select><br /><br />
+
+
+            </div>
+            <div className="OptionBookSubmit">
+                <input type="submit" id="OptionSubmit" value="Book" />
+            </div>
         </div>
     )
 }
