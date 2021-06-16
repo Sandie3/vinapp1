edb6504d62b20b84ddcb57b5f6fd0b621efbe0e3
Sandie3 <sandermunkh@gmail.com>
2021-06-16 13:50:06 +0200

    admin bookings

--- src/components/AdminBookings.jsx
+++ src/components/AdminBookings.jsx
@@ -6,8 +6,20 @@ const AdminBookings = () => {
             <input type="search" name="" id="adminSearch" className="adminSearch"/>
             <div className="adminBookings">
                 <div className="adminBook">
-
+                    <input type="text" name="" className="adminBookingNR" placeholder="Booking Number"/>
+                    <input type="text" name="" className="adminEmail" placeholder="Email"/>
+                    <input type="text" name="" className="adminName" placeholder="Name"/>
+                    <input type="text" name="" className="adminDate" placeholder="Date"/>
+                    <input type="text" name="" className="adminPhoneNR" placeholder="Phone Number"/>
+                    <button className="adminEditBooking">E</button>
+                    <button className="adminDelBooking">X</button>
                 </div>
+
+
+
+
+
+                
             </div>
         </>
     )
