31f69ed9934cfe8c867cb842f33506e1af440b90
rune-skaarup <79456478+rune-skaarup@users.noreply.github.com>
2021-06-17 10:20:02 +0200

    post

--- src/components/OptionContact.jsx
+++ src/components/OptionContact.jsx
@@ -1,21 +1,24 @@
 const OptionContact = () => {
-return (
-    <div className="OptionContact">
-            <div className="contactText">
-                <h3>Name<span>*</span>:</h3>
-                <input type="text" placeholder="Write your name..." /><br /><br />
-                <h3>Email<span>*</span>:</h3>
-                <input type="text" placeholder="Write your Email..." /><br /><br />
-                <h3>Phone<span>*</span>:</h3>
-                <input type="text" placeholder="Write your number..." />
-            </div>
-            <div className="contactTextArea">
-                <h3>Message<span>*</span>:</h3>
-                <textarea placeholder="Write your message..." rows="8.5" cols="25"></textarea><br /><br />
-            </div>
-            <div className="OptionContactSubmit">
-                <input type="submit" id="OptionSubmit" value="Submit" />
-            </div>
+
+    return (
+        <div>
+            <form action="#" className="OptionContact">
+                <div className="contactText">
+                    <h3>Name<span>*</span>:</h3>
+                    <input type="text" placeholder="Write your name..." /><br /><br />
+                    <h3>Email<span>*</span>:</h3>
+                    <input type="text" placeholder="Write your Email..." id="email"/><br /><br />
+                    <h3>Phone<span>*</span>:</h3>
+                    <input type="text" placeholder="Write your number..." />
+                </div>
+                <div className="contactTextArea">
+                    <h3>Message<span>*</span>:</h3>
+                    <textarea placeholder="Write your message..." rows="8.5" cols="25"></textarea><br /><br />
+                </div>
+                <div className="OptionContactSubmit">
+                    <input type="submit" id="OptionSubmit" value="Submit" />
+                </div>
+            </form>
         </div>
     )
 }
