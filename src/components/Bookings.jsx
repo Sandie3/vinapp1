aad7d15ec45bc187529133ebc7c3b9b2a5f8dab6
rune-skaarup <79456478+rune-skaarup@users.noreply.github.com>
2021-06-21 12:05:22 +0200

    vali

--- src/components/Bookings.jsx
+++ src/components/Bookings.jsx
@@ -7,13 +7,27 @@ import { getWinerie } from "../apicall/wineries"
 
 const Bookings = () => {
 
-    
     //state til svar fra api
     const [bookingData, setBookingData] = useState()
     const [error, setError] = useState()
     const [booking, setBooking] = useState()
     const [date, setDate] = useState(new Date());
-    
+    const [isValid, setIsValid] = useState(false);
+    const [message, setMessage] = useState('');
+
+    const emailRegex = /\S+@\S+\.\S+/;
+
+    const validateEmail = (event) => {
+        const email = event.target.value;
+        if (emailRegex.test(email)) {
+            setIsValid(true);
+            setMessage('Your email looks good!');
+        } else {
+            setIsValid(false);
+            setMessage('Please enter a valid email!');
+        }
+    };
+
     function onChange(nextValue) {
         setDate(nextValue);
     }
@@ -52,37 +66,37 @@ const Bookings = () => {
     }, [])
 
     const getRandomInt = (min = 0, max = 100) => {
-		return Math.floor(Math.random() * (max - min + 1)) + min;
-	}
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 
     useEffect(() => {
         generateRandom()
     }, [])
 
     const generateRandom = () => {
-		const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-		let number = '';
-		for (let i = 0; i < 12; i++) {
-			number += possible.charAt(getRandomInt(0, possible.length));
-		}
-		return setBooking(number);
-	}
+        const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        let number = '';
+        for (let i = 0; i < 12; i++) {
+            number += possible.charAt(getRandomInt(0, possible.length));
+        }
+        return setBooking(number);
+    }
 
     return (
         <div >
             <form onSubmit={handleSubmit} className="bookingOption">
                 <div className="bookingText">
                     <h3>Name<span>*</span>:</h3>
-                    <input type="text" placeholder="Write your name..." name="name"/><br /><br />
+                    <input type="text" placeholder="Write your name..." name="name" /><br /><br />
                     <h3>Email<span>*</span>:</h3>
-                    <input type="text" placeholder="Write your Email..." name="email"/><br /><br />
-                    <input type="text" name="booking" value={booking} style={{display:'none'}} /><br /><br />
-                    <input type="text" name="bookingDate" value={date} style={{display:'none'}} /><br /><br />
+                    <input type="email" placeholder="Write your Email..." name="email" onChange={validateEmail}/><br /><br />
+                    <input type="text" name="booking" value={booking} style={{ display: 'none' }} /><br /><br />
+                    <input type="text" name="bookingDate" value={date} style={{ display: 'none' }} /><br /><br />
 
                 </div>
                 <div className="bookingChoose">
                     <h3>Phone<span>*</span>:</h3>
-                    <input type="text" placeholder="Write your phone number..." name="phone"/><br /><br />
+                    <input type="text" placeholder="Write your phone number..." name="phone" /><br /><br />
                     <h3>Choose winery<span>*</span>:</h3>
                     <select name="winerie" id="selectBooking">
                         {
@@ -109,21 +123,21 @@ const Bookings = () => {
                     </select><br /><br />
                 </div>
                 <div className="kalender">
-                    <Calendar 
-                        onChange={onChange} 
+                    <Calendar
+                        onChange={onChange}
                         defaultValue={date}
                         // returnValue="range"
                         minDate={new Date()}
-                        // name="bookingDate"
-                        />
-                        {console.log(date)}
+                    // name="bookingDate"
+                    />
+                    {console.log(date)}
                 </div>
                 <div className="checkboxBook">
-                    <input type="checkbox" id="checkBooking" name="Check"/>
+                    <input type="checkbox" id="checkBooking" name="Check" />
                     <label htmlFor="checkBooking">Are you sure you want to book this date?</label>
                 </div>
                 <div className="bookOptionSubmit">
-                    <input type="submit" id="OptionSubmit" value="Book"/>
+                    <input type="submit" id="OptionSubmit" value="Book" />
                 </div>
             </form>
         </div>
