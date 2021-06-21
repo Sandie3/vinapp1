653b5c5a028c64cc313a0e8fb6a40a93090f859a
Sandie3 <sandermunkh@gmail.com>
2021-06-21 10:22:03 +0200

    APP DONE

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,20 +13,24 @@ const Home = () => {
         
 
         <div className="optionBox">
-            <h2>Our Wineries</h2>
             <Link to="/wineries">
+            <h2>Our Wineries</h2>
                 <img src="https://i.imgur.com/Ef7QHv3.png" alt="Tilfældig Vin Mark" />
             </Link>
         </div>
         
         <div className="optionBox">
-            <h2>TOM</h2>
-            <img src="https://i.imgur.com/CehHkqA.png" alt="Tilfældig Vin Mark" />
+            <Link to="/about">
+            <h2>About us</h2>
+                <img src="https://i.imgur.com/CehHkqA.png" alt="Tilfældig Vin Mark" />
+            </Link>
         </div>
 
         <div className="optionBox">
+            <Link to="/contact">
             <h2>Book a taste</h2>
-            <img src="https://i.imgur.com/XvaW9F6.png" alt="Tilfældig Vin Mark" />
+                <img src="https://i.imgur.com/XvaW9F6.png" alt="Tilfældig Vin Mark" />
+            </Link>
         </div>
 
 
